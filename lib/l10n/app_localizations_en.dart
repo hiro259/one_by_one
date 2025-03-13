84926c02838056da1a6bd425486f8862c75c1fbe
fa-bb <fabb1800@gmail.com>
2025-03-14 02:14:04 +0900

    create app_localizations.dart

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,34 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get tasktitle => 'Title';
+
+  @override
+  String get description => 'description';
+
+  @override
+  String get taskedit => 'Task Edit';
+
+  @override
+  String get taskNew => 'New Task';
+
+  @override
+  String get delete => 'Delete Task';
+
+  @override
+  String get save => 'Save Task';
+
+  @override
+  String get languages => 'Languages';
+
+  @override
+  String get colors => 'Colors';
+}
---
+++ lib/l10n/app_localizations_ja.dart
@@ -0,0 +1,34 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Japanese (`ja`).
+class AppLocalizationsJa extends AppLocalizations {
+  AppLocalizationsJa([String locale = 'ja']) : super(locale);
+
+  @override
+  String get tasktitle => 'タイトル';
+
+  @override
+  String get description => '詳細';
+
+  @override
+  String get taskedit => '編集';
+
+  @override
+  String get taskNew => '新規';
+
+  @override
+  String get delete => '削除';
+
+  @override
+  String get save => '保存';
+
+  @override
+  String get languages => '言語設定';
+
+  @override
+  String get colors => '色設定';
+}
